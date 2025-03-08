998feb6c47b5eaee4f052e373ab7358298f3263b
a1234 <@>
2025-03-08 18:10:38 +0700

    Initial commit before installing dependencies

--- src/stack_contract.sol
+++ src/stack_contract.sol
@@ -6,54 +6,51 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract StakingContract is ReentrancyGuard, Ownable {
-
     uint256 private constant SECONDS_PER_YEAR = 365 days;
-    uint256 private constant MAX_STAKING_PERIOD = 4 * SECONDS_PER_YEAR; 
+    uint256 private constant MAX_STAKING_PERIOD = 4 * SECONDS_PER_YEAR;
 
     IERC20 public stakingToken;
 
     uint256 public totalStaked;
 
-    
     mapping(address => Stake[]) internal userStaking;
 
     using EnumerableSet for EnumerableSet.AddressSet;
 
     // It is used for iterating over all stacks in the contract.
     EnumerableSet.AddressSet internal users;
-    
+
     struct Stake {
-        uint256 stakedAmount;    
-        uint256 withdrawDate;     
+        uint256 stakedAmount;
+        uint256 withdrawDate;
     }
-    
+
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
 
-    
     constructor(IERC20 _stakingToken) Ownable(msg.sender) {
         stakingToken = _stakingToken;
     }
-    
+
     /// @dev The contract allows you to add multiple stacks with different amounts and stacking time.
     function stake(uint256 amount, uint256 period) external nonReentrant {
         require(
-            period > 0 && period <= MAX_STAKING_PERIOD, 
+            period > 0 && period <= MAX_STAKING_PERIOD,
             "Staking period cannot exceed 4 years and must be greater than 0"
         );
-        require(
-            amount > 0, 
-            "Amount must be greater than 0"
-        );
+        require(amount > 0, "Amount must be greater than 0");
         Stake memory newStake = Stake(amount, block.timestamp + period);
         userStaking[msg.sender].push(newStake);
-        if (userStaking[msg.sender].length == 1){
+        if (userStaking[msg.sender].length == 1) {
             users.add(msg.sender);
         }
 
-        bool success = stakingToken.transferFrom(msg.sender, address(this), amount);
+        bool success = stakingToken.transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
         require(success, "Transfer failed");
         totalStaked += amount;
         emit Staked(msg.sender, amount);
@@ -68,39 +65,44 @@ contract StakingContract is ReentrancyGuard, Ownable {
 
         bool useHasActiveStack;
         for (uint i = 0; i < userStack.length; i++) {
-            if (userStack[i].withdrawDate <= block.timestamp && userStack[i].stakedAmount > 0){
+            if (
+                userStack[i].withdrawDate <= block.timestamp &&
+                userStack[i].stakedAmount > 0
+            ) {
                 unstackAmount += userStack[i].stakedAmount;
                 userStaking[msg.sender][i] = Stake(0, 0);
             }
-            if (!(userStack[i].withdrawDate <= block.timestamp) && userStack[i].stakedAmount > 0){
+            if (
+                !(userStack[i].withdrawDate <= block.timestamp) &&
+                userStack[i].stakedAmount > 0
+            ) {
                 useHasActiveStack = true;
             }
         }
-        if (!useHasActiveStack){
+        if (!useHasActiveStack) {
             users.remove(msg.sender);
         }
 
         require(unstackAmount > 0, "No unstack amount");
-        
+
         userStaking[msg.sender] = userStack;
         bool success = stakingToken.transfer(msg.sender, unstackAmount);
         require(success, "Transfer failed");
 
         totalStaked -= unstackAmount;
-        
+
         emit Withdrawn(msg.sender, unstackAmount);
     }
 
-    function getUserStake(address user) internal view returns(Stake[] memory) {
+    function getUserStake(address user) internal view returns (Stake[] memory) {
         return userStaking[user];
     }
 
-    function getStake() public view returns(Stake[] memory) {
+    function getStake() public view returns (Stake[] memory) {
         return userStaking[msg.sender];
     }
 
-    function getTotalStake() public view returns(uint256) {
+    function getTotalStake() public view returns (uint256) {
         return totalStaked;
     }
-
 }
--- test/stack_contract_test.sol
+++ test/stack_contract_test.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "stack_contract.sol";
+import "src/stack_contract.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
     constructor() ERC20("Mock Token", "MOCK") {
-        _mint(msg.sender, 1000000 * 10**18);
+        _mint(msg.sender, 1000000 * 10 ** 18);
     }
 }
 
@@ -18,26 +18,26 @@ contract StakingContractTest is Test {
     address public user1;
     address public user2;
 
-    uint256 constant INITIAL_SUPPLY = 1000000 * 10**18;
-    uint256 constant USER_BALANCE = 10000 * 10**18;
-    uint256 constant STAKE_AMOUNT = 1000 * 10**18;
+    uint256 constant INITIAL_SUPPLY = 1000000 * 10 ** 18;
+    uint256 constant USER_BALANCE = 10000 * 10 ** 18;
+    uint256 constant STAKE_AMOUNT = 1000 * 10 ** 18;
     uint256 constant STAKING_PERIOD = 7 days;
-    
+
     function setUp() public {
         owner = address(this);
         user1 = address(0x1);
         user2 = address(0x2);
-        
+
         mockToken = new MockToken();
         stakingContract = new StakingContract(address(mockToken));
-        
+
         mockToken.transfer(user1, USER_BALANCE);
         mockToken.transfer(user2, USER_BALANCE);
-        
+
         assertEq(mockToken.balanceOf(user1), USER_BALANCE);
         assertEq(mockToken.balanceOf(user2), USER_BALANCE);
     }
-    
+
     function testInitialState() public {
         assertEq(address(stakingContract.stakingToken()), address(mockToken));
         assertEq(stakingContract.getTotalStake(), 0);
@@ -66,41 +66,41 @@ contract StakingContractTest is Test {
         stakingContract.stake(0, STAKING_PERIOD);
         vm.stopPrank();
     }
-    
+
     function testStaking() public {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT);
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         vm.stopPrank();
-        
+
         assertEq(mockToken.balanceOf(address(stakingContract)), STAKE_AMOUNT);
         assertEq(mockToken.balanceOf(user1), USER_BALANCE - STAKE_AMOUNT);
-        
+
         assertEq(stakingContract.getTotalStake(), STAKE_AMOUNT);
-        
+
         vm.startPrank(user1);
         StakingContract.Stake[] memory stakes = stakingContract.getStake();
         vm.stopPrank();
-        
+
         assertEq(stakes.length, 1);
         assertEq(stakes[0].stakedAmount, STAKE_AMOUNT);
         assertEq(stakes[0].withdrawDate, block.timestamp + STAKING_PERIOD);
     }
-    
+
     function testMultipleStaking() public {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT * 3);
-        
+
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         stakingContract.stake(STAKE_AMOUNT * 2, STAKING_PERIOD * 2);
-        
+
         StakingContract.Stake[] memory stakes = stakingContract.getStake();
         vm.stopPrank();
-        
+
         assertEq(stakes.length, 2);
         assertEq(stakes[0].stakedAmount, STAKE_AMOUNT);
         assertEq(stakes[1].stakedAmount, STAKE_AMOUNT * 2);
-        
+
         assertEq(stakingContract.getTotalStake(), STAKE_AMOUNT * 3);
     }
 
@@ -109,25 +109,25 @@ contract StakingContractTest is Test {
         mockToken.approve(address(stakingContract), STAKE_AMOUNT);
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         vm.stopPrank();
-        
+
         vm.startPrank(user2);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT * 2);
         stakingContract.stake(STAKE_AMOUNT * 2, STAKING_PERIOD * 2);
         vm.stopPrank();
-        
+
         assertEq(stakingContract.getTotalStake(), STAKE_AMOUNT * 3);
-        
+
         vm.startPrank(user1);
         StakingContract.Stake[] memory stakes1 = stakingContract.getStake();
         vm.stopPrank();
-        
+
         vm.startPrank(user2);
         StakingContract.Stake[] memory stakes2 = stakingContract.getStake();
         vm.stopPrank();
-        
+
         assertEq(stakes1.length, 1);
         assertEq(stakes1[0].stakedAmount, STAKE_AMOUNT);
-        
+
         assertEq(stakes2.length, 1);
         assertEq(stakes2[0].stakedAmount, STAKE_AMOUNT * 2);
     }
@@ -139,7 +139,7 @@ contract StakingContractTest is Test {
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         vm.stopPrank();
     }
-    
+
     function testFailWithdrawZeroAmount() public {
         vm.startPrank(user1);
         stakingContract.withdraw();
@@ -150,35 +150,34 @@ contract StakingContractTest is Test {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT);
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
-        
+
         stakingContract.withdraw();
         vm.stopPrank();
     }
-    
+
     function testWithdrawAfterPeriod() public {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT);
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         vm.stopPrank();
-        
+
         vm.warp(block.timestamp + STAKING_PERIOD + 1);
-        
+
         vm.startPrank(user1);
         stakingContract.withdraw();
         vm.stopPrank();
-        
+
         assertEq(mockToken.balanceOf(address(stakingContract)), 0);
         assertEq(mockToken.balanceOf(user1), USER_BALANCE);
         assertEq(stakingContract.getTotalStake(), 0);
     }
-    
 
     function testStakeWithdrawStake() public {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT);
         stakingContract.stake(STAKE_AMOUNT, STAKING_PERIOD);
         vm.stopPrank();
-        
+
         vm.warp(block.timestamp + STAKING_PERIOD + 1);
 
         vm.startPrank(user1);
@@ -188,11 +187,11 @@ contract StakingContractTest is Test {
         vm.startPrank(user1);
         mockToken.approve(address(stakingContract), STAKE_AMOUNT * 2);
         stakingContract.stake(STAKE_AMOUNT * 2, STAKING_PERIOD * 2);
-        
+
         vm.startPrank(user2);
         StakingContract.Stake[] memory stakes2 = stakingContract.getStake();
         vm.stopPrank();
-        
+
         assertEq(stakes1.length, 1);
         assertEq(stakes1[0].stakedAmount, STAKE_AMOUNT * 2);
 
